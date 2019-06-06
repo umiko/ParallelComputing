baac5656f54b5c4709aa1f12ba33d70bc8d910fa
umiko <robertscholl.private@gmail.com>
2019-06-06 08:50:35 +0200

    better passing

--- MPI/ringPassing.c
+++ MPI/ringPassing.c
@@ -9,9 +9,10 @@ int main(int argc, char** argv){
     
     int rank;
     int size;
-    int loops = 50;
+    int loops = 10;
 
-    int my_size = 0;
+    int my_size = rank;
+    int recv=0;
     double timer = MPI_Wtime();
 
     MPI_Status status;
@@ -22,13 +23,14 @@ int main(int argc, char** argv){
     
     double timerAll = MPI_Wtime();
 
-    for (int i = 0; i < loops; i++)
+    MPI_Isend(&my_size, 1, MPI_INT, rank == size - 1 ? 0 : rank + 1, 256, MPI_COMM_WORLD, &sendreq);
+    for (int i = 0; i < loops-1; i++)
     {
-        MPI_Irecv(&my_size, 1, MPI_INT,rank==0 ? size-1 : rank-1, 256, MPI_COMM_WORLD, &recvreq);
-        my_size+=rank;
-        MPI_Isend(&my_size, 1, MPI_INT, rank==size-1 ? 0 : rank+1, 256, MPI_COMM_WORLD, &sendreq);
-        printf("Rank Sum: %i", my_size);
+        MPI_Irecv(&recv, 1, MPI_INT,rank==0 ? size-1 : rank-1, 256, MPI_COMM_WORLD, &recvreq);
+        my_size+=recv;
+        MPI_Isend(&recv, 1, MPI_INT, rank==size-1 ? 0 : rank+1, 256, MPI_COMM_WORLD, &sendreq);
     }
+    printf("Rank Sum: %i", my_size);
 
     MPI_Finalize();
     return 0;
