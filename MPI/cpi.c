9ad92cfa0abd2d86e955e44b345e115ee39671b5
umiko <robertscholl.private@gmail.com>
2019-07-04 10:55:51 +0200

    more rust

---
+++ MPI/cpi.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#define f(A) (4.0/(1.0+A*A)) 
+const int n = 1000000000; 
+int main(int argc, char* argv[]) {
+    int i;
+    double w, x, sum, pi;
+    w = 1.0/n;
+    sum = 0.0;
+    for (i=0; i<n; i++) {
+        x = w * ((double)i+ 0.5);
+        sum = sum + f(x);
+    }
+    printf("pi = %.15f\n", w*sum);
+    return 0;
+}
---
+++ rust/src/rustyPi2.rs
@@ -0,0 +1,14 @@
+const N: usize = 1_000_000_000;
+const W: f64 = 1f64/(N as f64);
+
+fn f(x: f64) -> f64 {
+    4.0/(1.0+x*x)
+}
+
+fn main(){
+    let sum : f64 = (0..N)
+        .into_iter()
+        .map(|i| f(W*((i as f64)+0.5)))
+        .sum::<f64>();
+    println!("pi = {}", W*sum);
+}
